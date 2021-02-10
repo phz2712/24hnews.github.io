75e2586587f2b5ff8562b6600fd63b7c4ddaa49f
phz2712 <lhp2712@gmail.com>
2021-02-10 11:40:25 +0700

    Updates

---
+++ precache-manifest.481149897fa928f8fedcf094bc371019.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed566372049960441abee5f7c756e33",
+    "url": "/24hnews.github.io/index.html"
+  },
+  {
+    "revision": "1cbd197d55a797566953",
+    "url": "/24hnews.github.io/static/css/2.626e111f.chunk.css"
+  },
+  {
+    "revision": "1cbd197d55a797566953",
+    "url": "/24hnews.github.io/static/js/2.7b470b49.chunk.js"
+  },
+  {
+    "revision": "7b1affef45e2b57a268c1974299a48a9",
+    "url": "/24hnews.github.io/static/js/2.7b470b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a580ce2ec61799c8cf",
+    "url": "/24hnews.github.io/static/js/main.d533f70a.chunk.js"
+  },
+  {
+    "revision": "4424d79268e2cc2563dd",
+    "url": "/24hnews.github.io/static/js/runtime-main.96ca7b67.js"
+  },
+  {
+    "revision": "f0b4cd0deec63cb1caafaeb78619e752",
+    "url": "/24hnews.github.io/static/media/1599839429-cogai_ganhcam.f0b4cd0d.jpg"
+  },
+  {
+    "revision": "ac6fab9b9968fd4e124517f1b887effd",
+    "url": "/24hnews.github.io/static/media/1600263667-nguyen_dion.ac6fab9b.jpg"
+  },
+  {
+    "revision": "700876f31cf10d0b8dc1738714e68326",
+    "url": "/24hnews.github.io/static/media/1600274563-dinhnai_pheavy.700876f3.jpg"
+  },
+  {
+    "revision": "6de4362fa35666660021fb10fef151ae",
+    "url": "/24hnews.github.io/static/media/1600328537-mike_tyson54t.6de4362f.jpg"
+  },
+  {
+    "revision": "2368335c1a8b3aef2da80cc9e48f698f",
+    "url": "/24hnews.github.io/static/media/1600338826-linhcau.2368335c.jpg"
+  },
+  {
+    "revision": "784857cb0228f5153fa8a54507f6a903",
+    "url": "/24hnews.github.io/static/media/1600354895-tauhaicucmanh.784857cb.jpg"
+  },
+  {
+    "revision": "7745515053d630d35af1f36c45198685",
+    "url": "/24hnews.github.io/static/media/1600399770-tuyen_map.77455150.jpg"
+  },
+  {
+    "revision": "6991730e4ad646a5431933f401d38c59",
+    "url": "/24hnews.github.io/static/media/1624337472769875971.6991730e.jpg"
+  },
+  {
+    "revision": "c4e1494e2a633be25ab3f167d17db7d3",
+    "url": "/24hnews.github.io/static/media/3650587649026593215.c4e1494e.jpg"
+  },
+  {
+    "revision": "614936c5dd9c15bd4340c35bee5300d8",
+    "url": "/24hnews.github.io/static/media/Tin tức bóng đá, thể thao, giải trí - Đọc tin tức 24h mới nhất (1).614936c5.mp4"
+  },
+  {
+    "revision": "9de1eb01de0fb6c14faf06649ae2f157",
+    "url": "/24hnews.github.io/static/media/XemLich.9de1eb01.png"
+  },
+  {
+    "revision": "b90174e42781144e09faa406e157c4ad",
+    "url": "/24hnews.github.io/static/media/a-1600440400-202-width640height480.b90174e4.jpg"
+  },
+  {
+    "revision": "a39483ae571d7959e13eca9bc428f1ec",
+    "url": "/24hnews.github.io/static/media/avt-as-width640height480.a39483ae.jpg"
+  },
+  {
+    "revision": "e29d37f6b419ac6fef6ab7312185b010",
+    "url": "/24hnews.github.io/static/media/avt1-1600156241-204-width640height480.e29d37f6.jpg"
+  },
+  {
+    "revision": "c03b8d187ffb19936dc66d1cfd8c15a3",
+    "url": "/24hnews.github.io/static/media/ba-445-thumbnail-width640height480.c03b8d18.jpg"
+  },
+  {
+    "revision": "0fa944074a1a72be158975c0c6bc1310",
+    "url": "/24hnews.github.io/static/media/cp_special_cup_quoc_gia-1600348381-viettel_hanoi.0fa94407.jpg"
+  },
+  {
+    "revision": "28752eda9a1f6cb511f86fe62251c4be",
+    "url": "/24hnews.github.io/static/media/cp_special_tennis_no_us-1600237812-paire_botran.28752eda.jpg"
+  },
+  {
+    "revision": "8fc72e22a69f8b224dc3c85a88720024",
+    "url": "/24hnews.github.io/static/media/download.8fc72e22.png"
+  },
+  {
+    "revision": "edfe77d917128fcf2800d1f0105cf3d2",
+    "url": "/24hnews.github.io/static/media/ewr9464139988557.edfe77d9.gif"
+  },
+  {
+    "revision": "51ab07b69f7dc67b395b2c9e2017d9fc",
+    "url": "/24hnews.github.io/static/media/image_dai_dien_gif2-1600421826-901-width640height480.51ab07b6.gif"
+  },
+  {
+    "revision": "4cce6809013d9f108876e331d11d8b9a",
+    "url": "/24hnews.github.io/static/media/lh-thumbnail-width640height480.4cce6809.jpg"
+  },
+  {
+    "revision": "6a534060e58b55edc9f4ea3e1c37c5fa",
+    "url": "/24hnews.github.io/static/media/opd-716-thumbnail-width640height480.6a534060.jpg"
+  },
+  {
+    "revision": "50103e6f9cdafc8ea9056368d6a6f5cc",
+    "url": "/24hnews.github.io/static/media/sd-thumbnail-width640height480.50103e6f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/24hnews.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/24hnews.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/24hnews.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/24hnews.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
